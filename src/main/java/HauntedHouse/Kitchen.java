41ab12ecb38bb091a471716f770e18487b3b7c1e
yhimanen <y.himanen@gmail.com>
2020-01-30 16:28:44 +0200

    go removed from kitchen description

--- src/main/java/HauntedHouse/Kitchen.java
+++ src/main/java/HauntedHouse/Kitchen.java
@@ -4,7 +4,7 @@ public class Kitchen implements Room {
 
     @Override
     public void introduceRoom() {
-        System.out.println("\ngoYou have entered the kitchen. You are greeted by the smell of something rotting.\n" +
+        System.out.println("\nYou have entered the kitchen. You are greeted by the smell of something rotting.\n" +
                 "There is a fridge quietly humming in the corner, a trash can, and an oven. Someone has left a pair of chopsticks lying around.\n" +
                 "You wonder where those keys could be...");
     }
